ad95013dda2a5c6cbc6f69c943cc0ddbe84443db
edwardscoolhackername <edwardwillems.willems10@gmail.com>
2021-03-25 14:39:54 -0400

    Another small fix to the winorlose function

--- game.py
+++ game.py
@@ -19,24 +19,27 @@ def winorlose(status):
 	# print("Inside winorlose function; status is ", status)
 	print ("You have ", status)
 	print("I demand a rematch. Do you dare try again?")
-	choice = input ("Y / N? ")
+	
+	choice = False
 
-	if choice == "N" or choice == "n":
-		print ("Oh? Scared you off, have I? Don't think your luck would last for another game? Very well, get your human stench away from my face.")
-		exit()
-	elif choice == "Y" or choice == "y":
-		global player_lives
-		global ai_lives
-		global total_lives
-
-		player_lives = total_lives
-		ai_lives = total_lives
-		print ("Very well. Prepare yourself; I will not go easy on you this time.")
-		
-	else: 
-		print ("I said Yes or No, fool. How did you get this far if you cannot read?")
-		# This could generate a bug to be fixed later. 
+	while choice = False:
 		choice = input ("Y / N? ")
+
+		if choice == "N" or choice == "n":
+			print ("Oh? Scared you off, have I? Don't think your luck would last for another game? Very well, get your human stench away from my face.")
+			exit()
+		elif choice == "Y" or choice == "y":
+			global player_lives
+			global ai_lives
+			global total_lives
+
+			player_lives = total_lives
+			ai_lives = total_lives
+			print ("Very well. Prepare yourself; I will not go easy on you this time.")
+			
+		else: 
+			print ("I said Yes or No, fool. How did you get this far if you cannot read?")
+			choice = False
 
 
 # player_choice == False
